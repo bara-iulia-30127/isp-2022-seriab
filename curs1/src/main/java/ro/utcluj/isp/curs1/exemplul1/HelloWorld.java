23d8581b270a731093bb7db81a22a4c5156f2cac
Mihai H <59961959+automatica-cluj@users.noreply.github.com>
2022-02-17 23:05:15 +0200

    Exemple curs 1.

---
+++ curs1/src/main/java/ro/utcluj/isp/curs1/exemplul1/HelloWorld.java
@@ -0,0 +1,11 @@
+
+package ro.utcluj.isp.curs1.exemplul1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello ISP, Curs 1");        
+    }
+    
+}
+
+
---
+++ curs1/src/main/java/ro/utcluj/isp/curs1/exemplul2/Test.java
@@ -0,0 +1,12 @@
+package ro.utcluj.isp.curs1.exemplul2;
+
+public class Test {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Test program");
+        while (true) {
+            System.out.println("It works...");
+            Thread.sleep(1000);
+        }
+    }
+}
